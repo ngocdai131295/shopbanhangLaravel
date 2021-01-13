6857b4a8c2bbe78932a86c32565cb0794571bec6
ngoc dai <ngocdai131295@gmail.com>
2021-01-13 15:50:35 +0700

    upload file

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('admin_layout')
+@section('admin_content')
+<h3><center>Chào mừng bạn đến với trang web bán hàng bằng laravel</center></h3>
+@endsection
---
+++ resources/views/pages/sanpham/lienhe.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+@section('content')
+<div class="features_items">
+                        <h2 class="title ">Thông tin liên hệ</h2>
+                        <div class="detail">
+                            <strong>Hệ thống trang web bán hàng bằng laravel </strong>
+                            <p><strong>Địa chỉ: </strong>+ 123 Lê Thị Riêng, P Bến Thành, Q1, HCM + 36 Nguyễn Hữu Cảnh, P22, Q Bình Thạnh, HCM + 28 Trần Trọng Cung, P Tân Thuận, Q7, HCM</p>
+                            <p><strong>Số điện thoại: </strong> 0987.654.321 - 0989.989.989</p>
+                            <p><strong>Email    : </strong> dailybanle@gmail.com</p>
+                        </div>
+</div>
+@endsection
